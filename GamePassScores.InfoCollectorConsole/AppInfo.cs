04a51fe3b8f2be07be707978e23be82b853e5af9
康宏嘉 <redalertkhj@live.cn>
2022-06-02 08:10:42 +0800

    Update InfoCollector version to 1.31.

--- GamePassScores.InfoCollectorConsole/AppInfo.cs
+++ GamePassScores.InfoCollectorConsole/AppInfo.cs
@@ -12,7 +12,7 @@ namespace GamePassScores.InfoCollectorConsole
         {
             get
             {
-                return "1.3";
+                return "1.31";
             }
         }
     }
